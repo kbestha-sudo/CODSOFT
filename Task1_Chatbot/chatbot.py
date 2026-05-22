cab25776d4f81b59e3713444cd8accc52ac13482
kbestha-sudo <kbestha@student.gitam.edu>
2026-05-22 14:34:15 +0530

    Add files via upload

---
+++ Task1_Chatbot/chatbot.py
@@ -0,0 +1,17 @@
+print("Simple AI Chatbot")
+print("Type 'bye' to exit")
+
+while True:
+    user = input("You: ").lower()
+
+    if user == "hello":
+        print("Bot: Hi there!")
+    elif user == "how are you":
+        print("Bot: I am fine.")
+    elif user == "what is your name":
+        print("Bot: I am an AI chatbot.")
+    elif user == "bye":
+        print("Bot: Goodbye!")
+        break
+    else:
+        print("Bot: Sorry, I don't understand.")
